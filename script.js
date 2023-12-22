c8edda0cfd503f7aa720b19e1cf01bc78c6d1a2f
Arif Hussain <arifh9821@gmail.com>
2023-12-22 15:36:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const extendHex = (shortHex) => {
   // write your code here
+	
+  // Remove # symbol if it exists
+  if (shortHex.startsWith('#')) {
+    shortHex = shortHex.slice(1);
+  }
+
+  // Expand short hex to full hex format
+  let fullHex = '#';
+  for (let i = 0; i < shortHex.length; i++) {
+    // Repeat each character to form the full hex code
+    fullHex += shortHex[i] + shortHex[i];
+  }
+
+  return fullHex;
+
+
 };
 
 // Do not change the code below.
